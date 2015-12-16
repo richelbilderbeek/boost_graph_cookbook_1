cb1517769fd89c6d488c489596c0556c629cfd7e
richelbilderbeek <richel@richelbilderbeek.nl>
2015-12-16 16:47:41 +0100

    Can load directed graphs with named edges

---
+++ load_directed_named_edges_and_vertices_graph_from_dot.cpp
@@ -0,0 +1,41 @@
+#include "load_directed_named_vertices_graph_from_dot.h"
+
+#include "load_directed_named_vertices_graph_from_dot.impl"
+
+#include "load_directed_named_vertices_graph_from_dot_demo.impl"
+
+#include "copy_file.h"
+#include <iostream>
+
+#include "create_named_vertices_markov_chain.h"
+#include "convert_dot_to_svg.h"
+#include "save_named_vertices_graph_to_dot.h"
+#include "fileio.h"
+
+void load_directed_named_vertices_graph_from_dot_test() noexcept
+{
+  //Create the picture 'load_directed_named_vertices_graph_from_dot_test_markov_chain.svg'
+  //Create graphs, save it to dot
+  //Create another graph by loading it, then save it to .dot, convert that .dot to .svg
+  {
+    const auto g = create_named_vertices_markov_chain();
+    const std::string dot_filename{"load_directed_named_vertices_graph_from_dot_test_markov_chain.dot"};
+    const std::string svg_filename{"load_directed_named_vertices_graph_from_dot_test_markov_chain.svg"};
+    save_named_vertices_graph_to_dot(g, dot_filename);
+    const auto old_text = ribi::FileIo().FileToVector(dot_filename);
+    const auto h = load_directed_named_vertices_graph_from_dot(dot_filename);
+    save_named_vertices_graph_to_dot(h, dot_filename);
+    const auto new_text = ribi::FileIo().FileToVector(dot_filename);
+    assert(old_text == new_text);
+    convert_dot_to_svg(dot_filename, svg_filename);
+    assert(boost::num_edges(g) == boost::num_edges(h));
+    assert(boost::num_vertices(g) == boost::num_vertices(h));
+    copy_file(
+      svg_filename,
+      "../BoostGraphTutorial/" + svg_filename,
+      copy_file_mode::allow_overwrite
+    );
+  }
+  load_directed_named_vertices_graph_from_dot_demo();
+  std::cout << __func__ << ": OK" << '\n';
+}
