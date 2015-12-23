fe5089e976ddfc506f3144cb65beeb373b90a282
richelbilderbeek <richel@richelbilderbeek.nl>
2015-12-23 16:12:57 +0100

    v1.2

--- BoostGraphTutorial/main.cpp
+++ BoostGraphTutorial/main.cpp
@@ -2,8 +2,6 @@
 #include "add_custom_edge.h"
 #include "add_custom_vertex.h"
 #include "add_edge.h"
-#include "create_nasty_directed_custom_edges_and_vertices_graph.h"
-#include "create_nasty_undirected_custom_edges_and_vertices_graph.h"
 #include "add_named_edge.h"
 #include "add_named_vertex.h"
 #include "add_vertex.h"
@@ -33,27 +31,28 @@
 #include "create_k2_graph_with_graph_name.h"
 #include "create_markov_chain.h"
 #include "create_markov_chain_with_graph_name.h"
-#include "create_nasty_directed_custom_vertices_graph.h"
 #include "create_named_edges_and_vertices_k3_graph.h"
 #include "create_named_edges_and_vertices_markov_chain.h"
 #include "create_named_vertices_k2_graph.h"
 #include "create_named_vertices_markov_chain.h"
+#include "create_nasty_directed_bundled_vertices_graph.h"
+#include "create_nasty_directed_custom_edges_and_vertices_graph.h"
+#include "create_nasty_directed_custom_vertices_graph.h"
+#include "create_nasty_undirected_custom_edges_and_vertices_graph.h"
 #include "create_tutorial_chapters_graph.h"
 #include "find_first_bundled_vertex_with_my_vertex.h"
-#include "find_first_custom_vertex_with_my_vertex.h"
 #include "find_first_custom_edge_with_my_edge.h"
-#include "graphviz_encode.h"
-#include "graphviz_decode.h"
+#include "find_first_custom_vertex_with_my_vertex.h"
 #include "find_first_edge_with_name.h"
 #include "find_first_vertex_with_name.h"
 #include "get_bundled_vertex_my_vertexes.h"
 #include "get_bundled_vertex_my_vertex.h"
+#include "get_custom_edge_my_edge.h"
 #include "get_custom_edge_my_edges.h"
 #include "get_custom_vertex_my_vertexes.h"
 #include "get_custom_vertex_my_vertex.h"
 #include "get_edge_descriptors.h"
 #include "get_edge_iterators.h"
-#include "get_custom_edge_my_edge.h"
 #include "get_edge_my_edges.h"
 #include "get_edge_name.h"
 #include "get_edge_names.h"
@@ -67,9 +66,11 @@
 #include "get_vertex_name.h"
 #include "get_vertex_names.h"
 #include "get_vertex_out_degrees.h"
+#include "graphviz_decode.h"
+#include "graphviz_encode.h"
 #include "has_bundled_vertex_with_my_vertex.h"
-#include "has_edge_between_vertices.h"
 #include "has_custom_edge_with_my_edge.h"
+#include "has_edge_between_vertices.h"
 #include "has_edge_with_name.h"
 #include "has_vertex_with_name.h"
 #include "helper.h"
@@ -96,7 +97,6 @@
 #include "property_not_found_node_id.h"
 #include "remove_edge_between_vertices_with_names.h"
 #include "remove_first_edge_with_name.h"
-#include "create_nasty_directed_bundled_vertices_graph.h"
 #include "remove_first_vertex_with_name.h"
 #include "save_bundled_vertices_graph_to_dot.h"
 #include "save_custom_edges_and_vertices_graph_to_dot.h"
@@ -108,9 +108,9 @@
 #include "seperate_string.h"
 #include "set_bundled_vertex_my_vertexes.h"
 #include "set_bundled_vertex_my_vertex.h"
+#include "set_custom_edge_my_edge.h"
 #include "set_custom_vertex_my_vertexes.h"
 #include "set_custom_vertex_my_vertex.h"
-#include "set_custom_edge_my_edge.h"
 #include "set_edge_name.h"
 #include "set_graph_name.h"
 #include "set_vertex_name.h"
@@ -127,7 +127,6 @@ int main()
   add_named_edge_test();
   add_named_vertex_test();
   add_vertex_test();
-  create_nasty_directed_custom_vertices_graph_test();
   clear_first_vertex_with_name_test();
   count_vertices_with_name_test();
   create_bundled_vertices_k2_graph_test();
@@ -136,13 +135,9 @@ int main()
   create_custom_edges_and_vertices_markov_chain_test();
   create_custom_vertices_k2_graph_test();
   create_custom_vertices_markov_chain_test();
-  graphviz_encode_test();
-  graphviz_decode_test();
   create_empty_directed_bundled_vertices_graph_test();
-  create_nasty_directed_bundled_vertices_graph_test();
   create_empty_directed_custom_edges_and_vertices_graph_test();
   create_empty_directed_custom_vertices_graph_test();
-  create_nasty_undirected_custom_edges_and_vertices_graph_test();
   create_empty_directed_graph_test();
   create_empty_directed_graph_with_graph_name_test();
   create_empty_directed_named_edges_and_vertices_graph_test();
@@ -161,20 +156,23 @@ int main()
   create_named_edges_and_vertices_markov_chain_test();
   create_named_vertices_k2_graph_test();
   create_named_vertices_markov_chain_test();
+  create_nasty_directed_bundled_vertices_graph_test();
+  create_nasty_directed_custom_vertices_graph_test();
+  create_nasty_undirected_custom_edges_and_vertices_graph_test();
   create_tutorial_chapters_graph_test();
   find_first_bundled_vertex_with_my_vertex_test();
-  find_first_custom_vertex_with_my_vertex_test();
   find_first_custom_edge_with_my_edge_test();
+  find_first_custom_vertex_with_my_vertex_test();
   find_first_edge_with_name_test();
   find_first_vertex_with_name_test();
   get_bundled_vertex_my_vertexes_test();
   get_bundled_vertex_my_vertex_test();
   get_custom_edge_my_edges_test();
   get_custom_edge_my_edges_test();
+  get_custom_edge_my_edge_test();
   get_custom_vertex_my_vertex_test();
   get_edge_descriptors_test();
   get_edge_iterators_test();
-  get_custom_edge_my_edge_test();
   get_edge_names_test();
   get_edge_name_test();
   get_first_vertex_with_name_out_degree_test();
@@ -187,10 +185,12 @@ int main()
   get_vertex_names_test();
   get_vertex_name_test();
   get_vertex_out_degrees_test();
+  graphviz_decode_test();
+  graphviz_encode_test();
   has_bundled_vertex_with_my_vertex_test();
+  has_custom_edge_with_my_edge_test();
   has_custom_vertex_with_my_vertex_test();
   has_edge_between_vertices_test();
-  has_custom_edge_with_my_edge_test();
   has_edge_with_name_test();
   has_vertex_with_name_test();
   helper();
@@ -227,14 +227,12 @@ int main()
   seperate_string_test();
   set_bundled_vertex_my_vertexes_test();
   set_bundled_vertex_my_vertex_test();
+  set_custom_edge_my_edge_test();
   set_custom_vertex_my_vertexes_test();
   set_custom_vertex_my_vertex_test();
-  set_custom_edge_my_edge_test();
   set_edge_name_test();
   set_graph_name_test();
   set_vertex_names_test();
   set_vertex_name_test();
   std::cout << "Done" << std::endl;
 }
-
-
