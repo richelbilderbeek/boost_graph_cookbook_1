3959a8575c0f7786af6d7f36ac88d2f5c99585f2
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-19 11:59:50 +0100

    Before testing script

--- BoostGraphTutorial/temp_create_test_files.sh
+++ BoostGraphTutorial/temp_create_test_files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for filename in `ls *_demo.impl`
+for filename in `ls *.cpp`
 do
   echo $filename
-  new_filename=`echo $filename | sed 's/_demo/_test/'`
+  new_filename=`echo $filename | sed 's/\.cpp/_test\.cpp/'`
   echo $new_filename
-  cp $filename $new_filename
+  #cp $filename $new_filename
 done
