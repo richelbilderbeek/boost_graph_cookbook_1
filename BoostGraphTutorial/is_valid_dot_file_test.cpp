6a07099e1cd8bda0dd538c2d8527898edd2d10ba
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-19 18:10:50 +0100

    Try to reproduce error on Travis

--- BoostGraphTutorial/is_valid_dot_file_test.cpp
+++ BoostGraphTutorial/is_valid_dot_file_test.cpp
@@ -4,39 +4,98 @@
 #include "create_empty_directed_graph.h"
 #include "create_empty_undirected_graph.h"
 #include "create_k2_graph.h"
+#include "create_k3_graph.h"
 #include "copy_file.h"
 #include "save_graph_to_dot.h"
 #include "helper.h"
 #include "is_regular_file.h"
 
-BOOST_AUTO_TEST_CASE(is_valid_dot_file_thorough)
+BOOST_AUTO_TEST_CASE(is_valid_dot_file_empty_undirected_graph)
 {
   //An empty undirected graph should be convertable to a valid .dot file
-  {
-    const auto g = create_empty_undirected_graph();
-    const std::string filename{
-      "is_valid_dot_file_test_create_empty_undirected_graph.dot"
-    };
-    save_graph_to_dot(g,filename);
-    BOOST_CHECK(is_valid_dot_file(filename));
-  }
-  //An empty directed graph should be convertable to a valid .dot file
-  {
-    const auto g = create_empty_directed_graph();
-    const std::string filename{
-      "is_valid_dot_file_test_create_empty_directed_graph.dot"
-    };
-    save_graph_to_dot(g,filename);
-    BOOST_CHECK(is_valid_dot_file(filename));
-  }
-  //A K2 graph should be convertable to a valid .dot file
-  {
-    const auto g = create_k2_graph();
-    const std::string filename{
-      "is_valid_dot_file_test_create_k2_graph.dot"
-    };
-    save_graph_to_dot(g,filename);
-    BOOST_CHECK(is_valid_dot_file(filename));
-  }
+  const auto g = create_empty_undirected_graph();
+  const std::string filename{
+    "is_valid_dot_file_test_create_empty_undirected_graph.dot"
+  };
+  save_graph_to_dot(g,filename);
+  BOOST_CHECK(is_valid_dot_file(filename));
+  std::remove(filename.c_str());
+}
 
+BOOST_AUTO_TEST_CASE(is_valid_dot_file_empty_directed_graph)
+{
+  //An empty directed graph should be convertable to a valid .dot file
+  const auto g = create_empty_directed_graph();
+  const std::string filename{
+    "is_valid_dot_file_test_create_empty_directed_graph.dot"
+  };
+  save_graph_to_dot(g,filename);
+  BOOST_CHECK(is_valid_dot_file(filename));
+  std::remove(filename.c_str());
+}
+
+BOOST_AUTO_TEST_CASE(is_valid_dot_file_k2_graph)
+{
+  //A K2 graph should be convertable to a valid .dot file
+  const auto g = create_k2_graph();
+  const std::string filename{
+    "is_valid_dot_file_test_create_k2_graph.dot"
+  };
+  save_graph_to_dot(g,filename);
+  BOOST_CHECK(is_valid_dot_file(filename));
+  std::remove(filename.c_str());
+}
+
+BOOST_AUTO_TEST_CASE(is_valid_dot_file_k3_graph)
+{
+  //A K2 graph should be convertable to a valid .dot file
+  const auto g = create_k3_graph();
+  const std::string filename{
+    "is_valid_dot_file_test_create_k2_graph.dot"
+  };
+  save_graph_to_dot(g,filename);
+  BOOST_CHECK(is_valid_dot_file(filename));
+  std::remove(filename.c_str());
+}
+
+BOOST_AUTO_TEST_CASE(is_valid_dot_file_20160319)
+{
+  {
+    const std::string filename{"test_20160319_1.dot"};
+    {
+      std::ofstream f(filename);
+      f
+        << "digraph G {\n"
+        << "name=\"Two-state Markov chain\";\n"
+        << "0;\n"
+        << "1;\n"
+        << "0->0 ;\n"
+        << "0->1 ;\n"
+        << "1->0 ;\n"
+        << "1->1 ;\n"
+        << "}\n" //Note: newline
+      ;
+    }
+    BOOST_CHECK(is_valid_dot_file(filename));
+    std::remove(filename.c_str());
+  }
+  {
+    const std::string filename{"test_20160319_2.dot"};
+    {
+      std::ofstream f(filename);
+      f
+        << "digraph G {\n"
+        << "name=\"Two-state Markov chain\";\n"
+        << "0;\n"
+        << "1;\n"
+        << "0->0 ;\n"
+        << "0->1 ;\n"
+        << "1->0 ;\n"
+        << "1->1 ;\n"
+        << "}" //No newline
+      ;
+    }
+    BOOST_CHECK(is_valid_dot_file(filename));
+    std::remove(filename.c_str());
+  }
 }
