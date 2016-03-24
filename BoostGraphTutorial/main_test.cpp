6abbaf210716acced33f0ab312a221eb09700953
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-24 17:24:58 +0100

    EOD

--- BoostGraphTutorial/main_test.cpp
+++ BoostGraphTutorial/main_test.cpp
@@ -3,13 +3,3 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE boost_graph_tutorial_test_module
 #include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(inform_travis)
-{
-  #ifndef NDEBUG
-  std::ofstream f("test_debug.txt");
-  #else
-  std::ofstream f("test_release.txt");
-  #endif
-  f << "OK\n";
-}
