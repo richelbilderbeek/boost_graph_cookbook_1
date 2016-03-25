757c886144a09ec1ed30fa0c505affd89fd648f0
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-25 14:06:54 +0100

    Reduce codecov verbosity?

--- get_code_cov.sh
+++ get_code_cov.sh
@@ -8,7 +8,4 @@ then
   exit 1
 fi
 
-for filename in `find . | egrep '\.cpp'`; do $GCOV_EXE -o $OBJ_FILES_PATH $filename; done
-
-# Display gcov files
-#for filename in `find . -name *.gcov`; do cat $filename; done
+for filename in `find . | egrep '\.cpp'`; do $GCOV_EXE -o $OBJ_FILES_PATH $filename > /dev/null; done
