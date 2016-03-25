c5435c5fdf0ed8806209d1f860e62417ae4c570c
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-25 14:52:22 +0100

    Do not do version control on the .DOT and .SVGs in root folder

--- clean.sh
+++ clean.sh
@@ -6,8 +6,8 @@ function clean {
   rm Makefile.Debug
   rm Makefile.Release
   rm *.pro.user
-  rm distancer_exe
-  rm distancer_test
+  rm boost_graph_tutorial_test
+  rm boost_graph_tutorial
   rm exe_debug.txt
   rm exe_release.txt
   rm test_debug.txt
@@ -16,10 +16,13 @@ function clean {
   rm -rf release
 }
 
+rm -rf build-boost_graph_tutorial-Desktop-Debug
+rm -rf build-boost_graph_tutorial-Desktop-Release
+rm -rf build-boost_graph_tutorial_test-Desktop-Debug
+rm -rf build-boost_graph_tutorial_test-Desktop-Release
+rm create_*
 clean
+
+
 cd src; clean; cd ..
 cd cpp; clean; cd ..
-rm -rf build-distancer_exe-Desktop-Debug
-rm -rf build-distancer_exe-Desktop-Release
-rm -rf build-distancer_test-Desktop-Debug
-rm -rf build-distancer_test-Desktop-Release
