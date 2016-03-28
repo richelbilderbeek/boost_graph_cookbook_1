5a4f72cc10de3bcef1b7218ab53ac124e7f4c257
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-28 17:24:10 +0200

    Add message

--- BoostGraphTutorial/create_markov_chain_with_graph_name_test.cpp
+++ BoostGraphTutorial/create_markov_chain_with_graph_name_test.cpp
@@ -32,25 +32,33 @@ BOOST_AUTO_TEST_CASE(create_markov_chain_with_graph_name_save_and_load_should_be
 
 BOOST_AUTO_TEST_CASE(create_markov_chain_with_graph_name_create_pictures)
 {
-  const auto g = create_markov_chain_with_graph_name();
-  const std::string base_filename{"create_markov_chain_with_graph_name"};
-  const std::string dot_filename{base_filename + ".dot"};
-  BOOST_CHECK(!is_regular_file(dot_filename));
-  save_graph_with_graph_name_to_dot(g, dot_filename);
-  BOOST_CHECK(is_regular_file(dot_filename));
-  BOOST_CHECK(is_valid_dot_file(dot_filename));
-  copy_file(
-    dot_filename,
-    "../BoostGraphTutorial/" + dot_filename,
-    copy_file_mode::allow_overwrite
-  );
-  const std::string svg_filename{base_filename + ".svg"};
-  convert_dot_to_svg(dot_filename, svg_filename);
-  copy_file(
-    svg_filename,
-    "../BoostGraphTutorial/" + svg_filename,
-    copy_file_mode::allow_overwrite
-  );
-  std::remove(dot_filename.c_str());
-  std::remove(svg_filename.c_str());
+  try
+  {
+    const auto g = create_markov_chain_with_graph_name();
+    const std::string base_filename{"create_markov_chain_with_graph_name"};
+    const std::string dot_filename{base_filename + ".dot"};
+    BOOST_CHECK(!is_regular_file(dot_filename));
+    save_graph_with_graph_name_to_dot(g, dot_filename);
+    BOOST_CHECK(is_regular_file(dot_filename));
+    BOOST_CHECK(is_valid_dot_file(dot_filename));
+    copy_file(
+      dot_filename,
+      "../BoostGraphTutorial/" + dot_filename,
+      copy_file_mode::allow_overwrite
+    );
+    const std::string svg_filename{base_filename + ".svg"};
+    convert_dot_to_svg(dot_filename, svg_filename);
+    copy_file(
+      svg_filename,
+      "../BoostGraphTutorial/" + svg_filename,
+      copy_file_mode::allow_overwrite
+    );
+    std::remove(dot_filename.c_str());
+    std::remove(svg_filename.c_str());
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    BOOST_CHECK(!"Should not get here");
+  }
 }
