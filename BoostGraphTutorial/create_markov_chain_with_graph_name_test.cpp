d637e5c6a83d43bb9b0d10c183e5b02004918026
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-29 20:52:37 +0200

    Added CHECKPOINTs

--- BoostGraphTutorial/create_markov_chain_with_graph_name_test.cpp
+++ BoostGraphTutorial/create_markov_chain_with_graph_name_test.cpp
@@ -34,16 +34,22 @@ BOOST_AUTO_TEST_CASE(create_markov_chain_with_graph_name_create_pictures)
 {
   try
   {
+    BOOST_TEST_PASSPOINT();
     const auto g = create_markov_chain_with_graph_name();
+    BOOST_TEST_PASSPOINT();
     const std::string base_filename{"create_markov_chain_with_graph_name"};
     const std::string dot_filename{base_filename + ".dot"};
     //Clean up
+    BOOST_TEST_PASSPOINT();
     if (is_regular_file(dot_filename))
     {
+      BOOST_TEST_PASSPOINT();
       std::remove(dot_filename.c_str());
     }
+    BOOST_TEST_PASSPOINT();
     BOOST_CHECK(!is_regular_file(dot_filename));
     save_graph_with_graph_name_to_dot(g, dot_filename);
+    BOOST_TEST_PASSPOINT();
     BOOST_CHECK(is_regular_file(dot_filename));
     BOOST_CHECK(is_valid_dot_file(dot_filename));
     copy_file(
@@ -51,19 +57,26 @@ BOOST_AUTO_TEST_CASE(create_markov_chain_with_graph_name_create_pictures)
       "../BoostGraphTutorial/" + dot_filename,
       copy_file_mode::allow_overwrite
     );
+    BOOST_TEST_PASSPOINT();
     const std::string svg_filename{base_filename + ".svg"};
     convert_dot_to_svg(dot_filename, svg_filename);
+    BOOST_TEST_PASSPOINT();
     copy_file(
       svg_filename,
       "../BoostGraphTutorial/" + svg_filename,
       copy_file_mode::allow_overwrite
     );
+    BOOST_TEST_PASSPOINT();
     std::remove(dot_filename.c_str());
+    BOOST_TEST_PASSPOINT();
     std::remove(svg_filename.c_str());
+    BOOST_TEST_PASSPOINT();
   }
   catch (std::exception& e)
   {
+    BOOST_TEST_PASSPOINT();
     std::cerr << e.what() << '\n';
     BOOST_CHECK(!"Should not get here");
   }
+  BOOST_TEST_PASSPOINT();
 }
