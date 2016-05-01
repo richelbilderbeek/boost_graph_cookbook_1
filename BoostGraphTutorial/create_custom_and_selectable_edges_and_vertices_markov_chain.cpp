ef3471d69a70407941160b5eb19804cf3a46a397
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-01 15:56:26 +0200

    EOD

--- BoostGraphTutorial/create_custom_and_selectable_edges_and_vertices_markov_chain.cpp
+++ BoostGraphTutorial/create_custom_and_selectable_edges_and_vertices_markov_chain.cpp
@@ -1,26 +1,2 @@
-//TODO: #include "create_nasty_directed_custom_and_selectable_edges_and_vertices_graph.h"
 #include "create_custom_and_selectable_edges_and_vertices_markov_chain.h"
 #include "create_custom_and_selectable_edges_and_vertices_markov_chain.impl"
-
-
-
-
-
-
-#include "convert_dot_to_svg.h"
-#include "copy_file.h"
-#include "file_to_vector.h"
-#include "get_edge_iterators.h"
-#include "get_vertex_iterators.h"
-#include "get_edge_descriptors.h"
-#include "get_vertex_descriptors.h"
-#include "get_vertex_names.h"
-#include "get_my_custom_vertexes.h"
-#include "get_vertex_selectednesses.h"
-#include "create_custom_and_selectable_edges_and_vertices_markov_chain.h"
-#include "save_custom_and_selectable_vertices_graph_to_dot.h"
-#include "save_custom_and_selectable_edges_and_vertices_graph_to_dot.h"
-#include "load_directed_custom_and_selectable_edges_and_vertices_graph_from_dot.h"
-#include "create_custom_and_selectable_edges_and_vertices_markov_chain.h"
-#include "load_directed_custom_edges_and_vertices_graph_from_dot.h"
-
