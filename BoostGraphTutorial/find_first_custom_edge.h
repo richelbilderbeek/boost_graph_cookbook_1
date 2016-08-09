81ea88dbaa8c4dc18bae2aee4b22c00b8eb6723c
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-09 10:57:27 +0200

    Added find_first_custom_edge, a predicate function

---
+++ BoostGraphTutorial/find_first_custom_edge.h
@@ -0,0 +1,6 @@
+#ifndef FIND_FIRST_CUSTOM_EDGE_H
+#define FIND_FIRST_CUSTOM_EDGE_H
+
+#include "find_first_custom_edge.impl"
+
+#endif // FIND_FIRST_CUSTOM_EDGE_H
---
+++ BoostGraphTutorial/find_first_custom_edge_test.cpp
@@ -0,0 +1,40 @@
+#include "find_first_custom_edge.h"
+#include "find_first_custom_edge_demo.impl"
+
+#include <boost/test/unit_test.hpp>
+#include "create_custom_edges_and_vertices_k3_graph.h"
+#include "create_empty_directed_custom_edges_and_vertices_graph.h"
+
+BOOST_AUTO_TEST_CASE(find_first_custom_edge_thorough)
+{
+  //Search for name
+  {
+    const auto g = create_custom_edges_and_vertices_k3_graph();
+    const auto predicate = [](const Edge& e) { return e.get_name() == "BC"; }
+    const auto ed = find_first_custom_edge(predicate, g);
+    const auto edge = get_my_custom_edge(ed, g);
+    BOOST_CHECK(edge.get_name() == "BC");
+    BOOST_CHECK(edge.get_description() == "second");
+  }
+  //Search for description
+  {
+    const auto g = create_custom_edges_and_vertices_k3_graph();
+    const auto predicate = [](const Edge& e) { return e.get_description() == "second"; }
+    const auto ed = find_first_custom_edge(predicate, g);
+    const auto edge = get_my_custom_edge(ed, g);
+    BOOST_CHECK(edge.get_name() == "BC");
+    BOOST_CHECK(edge.get_description() == "second");
+  }
+}
+
+
+BOOST_AUTO_TEST_CASE(find_first_custom_edge_when_edge_is_absent)
+{
+  const auto g = create_empty_directed_custom_edges_and_vertices_graph();
+  const auto predicate = [](const Edge& e) { return e.get_name() == "some absent name"; }
+  BOOST_CHECK_THROW(
+    find_first_custom_edge(preicate, g),
+    std::invalid_argument
+  );
+}
+
