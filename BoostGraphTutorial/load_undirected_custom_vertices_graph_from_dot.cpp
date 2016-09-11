8e3ff2629bc6be44725b6a862b5ae0ad7c6e9276
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-11 15:30:23 +0200

    EOD

--- BoostGraphTutorial/load_undirected_custom_vertices_graph_from_dot.cpp
+++ BoostGraphTutorial/load_undirected_custom_vertices_graph_from_dot.cpp
@@ -1,14 +1,2 @@
 #include "load_undirected_custom_vertices_graph_from_dot.h"
 #include "load_undirected_custom_vertices_graph_from_dot.impl"
-
-
-
-#include "copy_file.h"
-#include "create_custom_vertices_markov_chain.h"
-#include "convert_dot_to_svg.h"
-#include "save_custom_vertices_graph_to_dot.h"
-#include "file_to_vector.h"
-#include "my_custom_vertex.h"
-#include "install_vertex_custom_type.h"
-#include "create_custom_vertices_k2_graph.h"
-
