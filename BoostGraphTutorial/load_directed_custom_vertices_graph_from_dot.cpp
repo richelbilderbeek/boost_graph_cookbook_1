dc4d4416f7c76533839fc75b163b914a1df2388b
JorikdeBoer <j.de.boer.40@student.rug.nl>
2016-12-23 15:24:11 +0100

    don't use Graphiz in any_peregrine episode VI

--- BoostGraphTutorial/load_directed_custom_vertices_graph_from_dot.cpp
+++ BoostGraphTutorial/load_directed_custom_vertices_graph_from_dot.cpp
@@ -1,7 +1,8 @@
 #include "load_directed_custom_vertices_graph_from_dot.h"
+
+#ifndef BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 #include "load_directed_custom_vertices_graph_from_dot.impl"
-
-
+#endif // BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 
 #include "copy_file.h"
 
--- BoostGraphTutorial/load_directed_custom_vertices_graph_from_dot.h
+++ BoostGraphTutorial/load_directed_custom_vertices_graph_from_dot.h
@@ -1,6 +1,8 @@
 #ifndef LOAD_DIRECTED_CUSTOM_VERTICES_GRAPH_FROM_DOT_H
 #define LOAD_DIRECTED_CUSTOM_VERTICES_GRAPH_FROM_DOT_H
 
+#ifndef BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
+
 #include "create_empty_directed_custom_vertices_graph.h"
 
 boost::adjacency_list<
@@ -14,5 +16,7 @@ boost::adjacency_list<
 load_directed_custom_vertices_graph_from_dot(
   const std::string& dot_filename
 );
+
+#endif // BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 
 #endif // LOAD_DIRECTED_CUSTOM_VERTICES_GRAPH_FROM_DOT_H
