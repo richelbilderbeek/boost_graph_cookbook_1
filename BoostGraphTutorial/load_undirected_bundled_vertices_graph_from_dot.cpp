727b5d6e991d7bcb567ede7d34098417f8a02f64
JorikdeBoer <j.de.boer.40@student.rug.nl>
2016-12-23 15:02:43 +0100

    don't use Graphiz in any_peregrine episode V

--- BoostGraphTutorial/load_undirected_bundled_vertices_graph_from_dot.cpp
+++ BoostGraphTutorial/load_undirected_bundled_vertices_graph_from_dot.cpp
@@ -1,5 +1,7 @@
 #include "load_undirected_bundled_vertices_graph_from_dot.h"
+#ifndef BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 #include "load_undirected_bundled_vertices_graph_from_dot.impl"
+#endif //BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 
 
 
--- BoostGraphTutorial/load_undirected_bundled_vertices_graph_from_dot.h
+++ BoostGraphTutorial/load_undirected_bundled_vertices_graph_from_dot.h
@@ -1,6 +1,8 @@
 #ifndef LOAD_UNDIRECTED_BUNDLED_VERTICES_GRAPH_FROM_DOT_H
 #define LOAD_UNDIRECTED_BUNDLED_VERTICES_GRAPH_FROM_DOT_H
 
+#ifndef BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
+
 #include "create_empty_undirected_bundled_vertices_graph.h"
 
 boost::adjacency_list<
@@ -12,5 +14,7 @@ boost::adjacency_list<
 load_undirected_bundled_vertices_graph_from_dot(
   const std::string& dot_filename
 );
+
+#endif //BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 
 #endif // LOAD_UNDIRECTED_BUNDLED_VERTICES_GRAPH_FROM_DOT_H
