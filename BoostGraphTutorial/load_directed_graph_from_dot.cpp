07abddf521fc294333172457f143f659d63b3962
JorikdeBoer <j.de.boer.40@student.rug.nl>
2016-12-23 14:18:33 +0100

    Also remove in .cpp, thanks @richelbilderbeek

--- BoostGraphTutorial/load_directed_graph_from_dot.cpp
+++ BoostGraphTutorial/load_directed_graph_from_dot.cpp
@@ -1,8 +1,5 @@
 #include "load_directed_graph_from_dot.h"
+
+#ifndef BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 #include "load_directed_graph_from_dot.impl"
-
-#include "copy_file.h"
-#include "create_markov_chain.h"
-#include "convert_dot_to_svg.h"
-#include "save_graph_to_dot.h"
-
+#endif // BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
