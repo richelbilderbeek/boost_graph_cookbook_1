ef0d787acb9b72a655329f25f435a9d13e6e4fb3
JorikdeBoer <j.de.boer.40@student.rug.nl>
2016-12-23 14:24:14 +0100

    don't use Graphiz in any_peregrine episode II

--- BoostGraphTutorial/load_directed_graph_with_graph_name_from_dot.cpp
+++ BoostGraphTutorial/load_directed_graph_with_graph_name_from_dot.cpp
@@ -1,14 +1,5 @@
 #include "load_directed_graph_with_graph_name_from_dot.h"
+
+#ifndef BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 #include "load_directed_graph_with_graph_name_from_dot.impl"
-
-
-
-#include "get_graph_name.h"
-#include "copy_file.h"
-#include "save_graph_with_graph_name_to_dot.h"
-#include "save_graph_with_graph_name_to_dot.h"
-#include "create_markov_chain_with_graph_name.h"
-#include "convert_dot_to_svg.h"
-#include "file_to_vector.h"
-#include "set_graph_name.h"
-
+#endif // BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
--- BoostGraphTutorial/load_directed_graph_with_graph_name_from_dot.h
+++ BoostGraphTutorial/load_directed_graph_with_graph_name_from_dot.h
@@ -1,6 +1,8 @@
 #ifndef LOAD_DIRECTED_GRAPH_WITH_GRAPH_NAMES_FROM_DOT_H
 #define LOAD_DIRECTED_GRAPH_WITH_GRAPH_NAMES_FROM_DOT_H
 
+#ifndef BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
+
 #include "create_empty_directed_graph_with_graph_name.h"
 
 boost::adjacency_list<
@@ -16,5 +18,7 @@ boost::adjacency_list<
 load_directed_graph_with_graph_name_from_dot(
   const std::string& dot_filename
 );
+
+#endif // BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 
 #endif // LOAD_DIRECTED_GRAPH_WITH_GRAPH_NAMES_FROM_DOT_H
