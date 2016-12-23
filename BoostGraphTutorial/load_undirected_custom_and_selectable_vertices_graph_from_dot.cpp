8a238ec2f71c6ee17731f17a01571719d95c248a
JorikdeBoer <j.de.boer.40@student.rug.nl>
2016-12-23 15:05:31 +0100

    don't use Graphiz in any_peregrine episode V

--- BoostGraphTutorial/load_undirected_custom_and_selectable_vertices_graph_from_dot.cpp
+++ BoostGraphTutorial/load_undirected_custom_and_selectable_vertices_graph_from_dot.cpp
@@ -1,6 +1,7 @@
 #include "load_undirected_custom_and_selectable_vertices_graph_from_dot.h"
+#ifndef BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 #include "load_undirected_custom_and_selectable_vertices_graph_from_dot.impl"
-
+#endif //BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 
 
 
