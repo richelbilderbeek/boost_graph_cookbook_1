5b4bf8b62d18d471eaeb4878d39feaf492379b3c
JorikdeBoer <j.de.boer.40@student.rug.nl>
2016-12-23 15:07:53 +0100

    don't use Graphiz in any_peregrine episode V

--- BoostGraphTutorial/load_undirected_custom_edges_and_vertices_graph_from_dot.cpp
+++ BoostGraphTutorial/load_undirected_custom_edges_and_vertices_graph_from_dot.cpp
@@ -1,6 +1,7 @@
 #include "load_undirected_custom_edges_and_vertices_graph_from_dot.h"
+#ifndef BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 #include "load_undirected_custom_edges_and_vertices_graph_from_dot.impl"
-
+#endif //BOOST_GRAPH_TUTORIAL_NO_GRAPHVIZ
 
 
 #include "copy_file.h"
