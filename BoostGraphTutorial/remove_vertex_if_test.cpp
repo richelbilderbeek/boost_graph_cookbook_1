5cf6cc399ca9cc50769339ff1f03a89fed81995c
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-29 09:16:15 +0200

    Fixed incorrect assert

--- BoostGraphTutorial/remove_vertex_if_test.cpp
+++ BoostGraphTutorial/remove_vertex_if_test.cpp
@@ -15,7 +15,7 @@ BOOST_AUTO_TEST_CASE(remove_vertex_on_path_graph)
   //
   auto g = create_path_graph(3);
   assert(boost::num_vertices(g) == 3);
-  assert(boost::num_edges(g) == 3);
+  assert(boost::num_edges(g) == 2);
   remove_vertex_if(
     [](const auto vd, const auto& my_graph)
     {
