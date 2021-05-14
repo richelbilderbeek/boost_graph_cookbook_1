419e0a8b4ef6f09251250e1598aeeb1247d92121
richelbilderbeek <richel@richelbilderbeek.nl>
2021-05-14 17:00:28 +0200

    Add TikZ image

--- edit.sh
+++ edit.sh
@@ -2,7 +2,9 @@
 mousepad \
   boost_graph_cookbook_1/boost_graph_cookbook_1.tex \
   boost_graph_cookbook_1/boost_graph_cookbook_1_chapter_1.tex \
-  boost_graph_cookbook_1/boost_graph_cookbook_1_chapter_2.tex
+  boost_graph_cookbook_1/boost_graph_cookbook_1_chapter_2.tex \
+  boost_graph_cookbook_1/boost_graph_cookbook_1_lyx.txt
+
 ./create_pdf.sh
 
 FILE=boost_graph_cookbook_1/boost_graph_cookbook_1.pdf
