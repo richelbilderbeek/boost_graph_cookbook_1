7c33db5067ffa017db4a99c68cd7e0b0b42ee2ec
richelbilderbeek <richel@richelbilderbeek.nl>
2021-05-16 10:49:43 +0200

    Work on chapter 4

--- edit.sh
+++ edit.sh
@@ -4,6 +4,7 @@ mousepad \
   boost_graph_cookbook_1/boost_graph_cookbook_1_chapter_1.tex \
   boost_graph_cookbook_1/boost_graph_cookbook_1_chapter_2.tex \
   boost_graph_cookbook_1/boost_graph_cookbook_1_chapter_3.tex \
+  boost_graph_cookbook_1/boost_graph_cookbook_1_chapter_4.tex \
   boost_graph_cookbook_1/boost_graph_cookbook_1_lyx.txt
 
 ./create_pdf.sh
